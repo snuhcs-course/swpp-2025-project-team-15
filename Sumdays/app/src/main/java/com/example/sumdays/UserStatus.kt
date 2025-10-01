6de9393154c84cb43341f9e7299a3d35b72aa615
mingu <mingu822@snu.ac.kr>
2025-10-01 16:52:22 +0900

    Revert "Merge pull request #3 from snuhcs-course/feature/loginFeature"
    This reverts commit f109a0f2c9c0d7ab87dd8a727707aa269f0c916d, reversing
    changes made to 6451777ce75de4732b2db8d4c5f2d67789396ace.

--- Sumdays/app/src/main/java/com/example/sumdays/UserStatus.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sumdays
-
-class UserStatus {
-    private val userId = "user";
-    fun getUserId(): String {
-        return userId;
-    }
-}
--- Sumdays/app/src/main/java/com/example/sumdays/network/LoginRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sumdays.network
-
-// 서버로 보낼 로그인 요청 데이터 형식
-data class LoginRequest(
-    val email: String,
-    val password: String
-)

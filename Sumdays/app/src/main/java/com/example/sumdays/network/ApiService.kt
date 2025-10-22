d503d4ca265602443371ac19e3761377b3744710
Younguk Jo <whduddnr0513@gmail.com>
2025-10-22 14:15:06 +0900

    flask 서버에 직접 호출하지 않고 js 서버를 거쳐서 호출하게 함. POST 위치도 수정.

--- Sumdays/app/src/main/java/com/example/sumdays/network/ApiService.kt
+++ Sumdays/app/src/main/java/com/example/sumdays/network/ApiService.kt
@@ -12,10 +12,10 @@ import retrofit2.http.Multipart
 import retrofit2.http.Part
 
 interface ApiService {
-    @POST("merge/")
+    @POST("/api/ai/merge/")
     suspend fun mergeMemos(@Body req: MergeRequest): retrofit2.Response<com.google.gson.JsonObject>
     @Multipart
-    @POST("/stt/memo") // 서버의 STT 엔드포인트 경로
+    @POST("/api/ai/stt/memo") // 서버의 STT 엔드포인트 경로
     fun transcribeAudio(
         // 'audio'는 서버에서 파일을 받을 때 사용할 필드 이름
         @Part audio: MultipartBody.Part

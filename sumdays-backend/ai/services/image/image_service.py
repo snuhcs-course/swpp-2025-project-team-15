2896f102155458560af2bd534e6fd1f233567195
Haneul Kim <skyski0716@gmail.com>
2025-10-23 14:32:58 +0900

    update: ocr api openai 사용하던 것을 google cloud vision을 쓰는 것으로 수정함.

--- sumdays-backend/ai/services/image/image_service.py
+++ sumdays-backend/ai/services/image/image_service.py
@@ -1,7 +1,10 @@
 from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage
+from google.cloud import vision
 import os
 import base64
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 class ImageService:
     """ Service for image memos or diaries """
@@ -10,15 +13,30 @@ class ImageService:
             model=os.getenv("IMAGE_MODEL", "gpt-4o-mini"),
             temperature=0.3
         )
+        self.ocr_client = vision.ImageAnnotatorClient()
 
     def analyze(self, image_file, analysis_type: str):
         """ Extract image descriptions or text depending on the analysis type """
         if analysis_type == "extract":
-            prompt = """
-            Extract all readable text from this image accurately.
-            Do not add any explanations, introductions, or quotes.
-            Output only the plain extracted text exactly as it appears in the image.
-            """
+            image_bytes = image_file.read()
+            image = vision.Image(content=image_bytes)
+
+            response = self.ocr_client.document_text_detection(image=image)
+            texts = response.text_annotations
+
+            if not texts:
+                return {
+                    "type": "extract",
+                    "text": ""
+                }
+
+            if response.error.message:
+                raise Exception(f"Vision API Error: {response.error.message}")
+            
+            return {
+                    "type": analysis_type,
+                    "text": texts[0].description.strip()
+                }
         elif analysis_type == "describe":
             prompt = """
             You are helping an app that analyzes a single diary image input.
@@ -31,23 +49,25 @@ class ImageService:
             (e.g., “listening to music” instead of “an audio player on a desk”)
             - Avoid excessive imagination or details not clearly implied by the image.
             """
+
+            image_bytes = image_file.read()
+            image_b64 = base64.b64encode(image_bytes).decode("utf-8")
+            data_uri = f"data:image/jpeg;base64,{image_b64}"
+
+            response = self.model.invoke([
+                HumanMessage(content=[
+                    {"type": "text", "text": prompt},
+                    {"type": "image_url", "image_url": {'url': data_uri}}
+                ])
+            ])
+
+            result_text = response.content.strip()
+
+            return {
+                "type": analysis_type,
+                "text": result_text
+            }
+        
         else:
             raise ValueError(f"Unsupported analysis type: {analysis_type}")
 
-        image_bytes = image_file.read()
-        image_b64 = base64.b64encode(image_bytes).decode("utf-8")
-        data_uri = f"data:image/jpeg;base64,{image_b64}"
-
-        response = self.model.invoke([
-            HumanMessage(content=[
-                {"type": "text", "text": prompt},
-                {"type": "image_url", "image_url": {'url': data_uri}}
-            ])
-        ])
-
-        result_text = response.content.strip()
-
-        return {
-            "type": analysis_type,
-            "text": result_text
-        }

1050d826277fb2fa72977f59c1a87c8d31905575
spklapjs <spklapjs@snu.ac.kr>
2025-11-05 16:04:14 +0900

    sdkmin = 24

--- Sumdays/app/build.gradle.kts
+++ Sumdays/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     }
     defaultConfig {
         applicationId = "com.example.first"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"

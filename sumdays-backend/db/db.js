7a964f720af67e2e472bbfbecfe6ed6d4ad8cbeb
mingu <mingu822@snu.ac.kr>
2025-11-22 20:45:19 +0900

    db folder merge

--- sumdays-backend/db/db.js
+++ sumdays-backend/db/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise'); // MySQL 데이터베이스와 비동기 통신을 위한 라이브러리
 
 const dbConfig = {
-    host: 'sumdays-database.c502cecwedjd.ap-northeast-2.rds.amazonaws.com',      // 예: sumdays-db.abcdefg12345.ap-northeast-2.rds.amazonaws.com
-    user: 'swpp_team15',       // 예: admin
-    password: 'aoij*i9!jUjkm',   // RDS 생성 시 설정한 마스터 암호
+    host: 'database-1.cjkge2608sxi.ap-northeast-2.rds.amazonaws.com',      // 예: sumdays-db.abcdefg12345.ap-northeast-2.rds.amazonaws.com
+    user: 'swpp_admin',       // 예: admin
+    password: 'kimstyle123##',   // RDS 생성 시 설정한 마스터 암호
     database: 'sumdays_db',         // 연결할 데이터베이스 이름
     waitForConnections: true,
     connectionLimit: 10,
@@ -12,6 +12,9 @@ const dbConfig = {
 
 // 데이터베이스 커넥션 풀(Pool) 생성. 여러 요청을 효율적으로 처리합니다.
 const pool = mysql.createPool(dbConfig);
-const post_pool = mysql.createPool(dbConfig);
 
-module.exports = pool;
+// ⭐ 반드시 둘 다 export해야 initDB.js에서 pool과 dbConfig 둘 다 사용 가능
+module.exports = {
+  pool,
+  dbConfig
+};
